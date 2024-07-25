0eef1f6d236ac988ba6a990f4a4c1d7ae6dad350
MacPhyle <macphyle@gmail>
2024-07-24 20:19:06 -0700

    Show Wriders

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -8,33 +8,71 @@ const Home = (props) => {
 
   const URL = "http://localhost:4000/wrider/";
 
-  const getWrider = async () => {
+  //fetches all wriders from our API backend
+  const getWriders = async () => {
     const response = await fetch(URL);
     const data = await response.json();
     setWrider(data.data);
   };
 
   const createWrider = async (wrider) => {
+    //make post request to creat wriders
     await fetch(URL, {
-      method: "post",
+      method: "POST",
       // What purpose does this serve?
+      // A: This headers code is part of the POST request, and it tells the server what kind of data is being sent.
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(wrider),
     });
-    getWrider();
+    // update our components list of wriders
+    getWriders();
+  };
+
+  const updateWriders = async (wrider, id) => {
+    // make post request to create wriders
+    await fetch(URL + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(wrider),
+    });
+    //update list of wriders
+    getWriders();
+  };
+
+  const deleteWriders = async (id) => {
+    // make post request to delete wriders
+    await fetch(URL + id, {
+      method: "DELETE",
+    });
+    //update list of wriders
+    getWriders();
   };
 
   useEffect(() => {
-    getWrider()
+    getWriders();
   }, []);
 
   return (
     <main>
       <Routes>
-        <Route path="/" element={<Index wrider={wrider} createWrider={createWrider} />} />
-        <Route path="/wrider/:id" element={<Show />} />
+        <Route
+          path="/"
+          element={<Index wrider={wrider} createWrider={createWrider} />}
+        />
+        <Route
+          path="/wrider/:id"
+          element={
+            <Show
+              wrider={wrider}
+              updateWrider={updateWrider}
+              deleteWriders={deleteWriders}
+            />
+          }
+        />
       </Routes>
     </main>
   );
