d28129efeb5641a327c49d00a1266f304516f1bd
Nadia Gushin <nadiagushin@Nadias-MacBook-Pro.local>
2024-07-27 16:47:49 -0400

    Update Home Page

--- frontend/src/components/WridersHome.jsx
+++ frontend/src/components/WridersHome.jsx
@@ -1,83 +1,40 @@
-/////Christopher's code going in this file
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import { useEffect, useState } from "react";
-import { Route, Routes } from "react-router-dom";
-import Index from "../pages/Index";
-import Show from "../pages/Show";
+const WridersHome = () => {
+    const [entries, setEntries] = useState([]);
 
-const Home = (props) => {
-  const [wrider, setWrider] = useState(null);
+    //Once app is deployed, change URL from http: to https: (secured)
+    // const URL = 'http://localhost:4000/wrider';
+    // const URL = process.env.REACT_APP_API_URL;
+    const URL = import.meta.env.VITE_APP_API_URL;
 
-  const URL = "http://localhost:4000/wrider/";
+    const fetchEntries = async () => {
+        try {
+            const response = await fetch(`${URL}/wrider`);
+            const data = await response.json();
+            setEntries(data);
+        } catch (error) {
+            console.error('Failed to fetch entries', error);
+        }
+    };
 
-  //fetches all wriders from our API backend
-  const getWriders = async () => {
-    const response = await fetch(URL);
-    const data = await response.json();
-    setWrider(data.data);
-  };
+    useEffect(() => {
+        fetchEntries();
+    }, []);
 
-  const createWrider = async (wrider) => {
-    //make post request to creat wriders
-    await fetch(URL, {
-      method: "POST",
-      // What purpose does this serve?
-      // A: This headers code is part of the POST request, and it tells the server what kind of data is being sent.
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(wrider),
-    });
-    // update our components list of wriders
-    getWriders();
-  };
-
-  const updateWriders = async (wrider, id) => {
-    // make post request to create wriders
-    await fetch(URL + id, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(wrider),
-    });
-    //update list of wriders
-    getWriders();
-  };
-
-  const deleteWriders = async (id) => {
-    // make post request to delete wriders
-    await fetch(URL + id, {
-      method: "DELETE",
-    });
-    //update list of wriders
-    getWriders();
-  };
-
-  useEffect(() => {
-    getWriders();
-  }, []);
-
-  return (
-    <main>
-      <Routes>
-        <Route
-          path="/"
-          element={<Index wrider={wrider} createWrider={createWrider} />}
-        />
-        <Route
-          path="/wrider/:id"
-          element={
-            <Show
-              wrider={wrider}
-              updateWrider={updateWrider}
-              deleteWriders={deleteWriders}
-            />
-          }
-        />
-      </Routes>
-    </main>
-  );
+    return (
+        <div>
+            <h1>Welcome back, Christopher</h1>
+            <ul>
+                {entries.map(entry => (
+                    <li key={entry._id}>
+                    <Link to={`/wrider/${entry._id}`}>{entry.title}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
-export default Home;
+export default WridersHome;
